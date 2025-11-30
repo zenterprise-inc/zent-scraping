d8e0d82c0f5156463efdfc4204558baa524387f7
rudy <rudy.kim@zent.kr>
2025-11-30 23:56:52 +0900

    feat: socket client

---
+++ online_scraping/lambda/websocket/events.ts
@@ -0,0 +1,4 @@
+export const SOCKET_EVENTS = {
+  SEND_MESSAGE: 'send_message',
+  RECEIVE_MESSAGE: 'message',
+} as const;
---
+++ online_scraping/lambda/websocket/socket-client.ts
@@ -0,0 +1,44 @@
+import { io, Socket } from 'socket.io-client';
+import { SOCKET_EVENTS } from './events';
+
+export class SocketClient {
+  private socket: Socket | null = null;
+
+  constructor(private token: string) {}
+
+  connect() {
+    if (this.socket) return this.socket;
+
+    this.socket = io('http://localhost:3000/chat', {
+      transports: ['websocket'],
+      extraHeaders: {
+        Authorization: `Bearer ${this.token}`,
+      },
+    });
+
+    this.socket.on('connect', () => {
+      console.log('Socket connected:', this.socket?.id);
+    });
+
+    this.socket.on('disconnect', () => {
+      console.log('Socket disconnected');
+    });
+
+    return this.socket;
+  }
+
+  sendMessage(message: string) {
+    if (!this.socket) throw new Error('Socket not connected');
+    this.socket.emit(SOCKET_EVENTS.SEND_MESSAGE, { message });
+  }
+
+  onMessage(cb: (msg: any) => void) {
+    if (!this.socket) throw new Error('Socket not connected');
+    this.socket.on(SOCKET_EVENTS.RECEIVE_MESSAGE, cb);
+  }
+
+  disconnect() {
+    this.socket?.disconnect();
+    this.socket = null;
+  }
+}
