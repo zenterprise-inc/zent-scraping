81223f407731ff94cdcbfe0d3711e8a74ac2dd2a
rudy <rudy.kim@zent.kr>
2025-12-16 15:04:24 +0900

    feat: header 추가

--- online_scraping/lambda/scrape_wright.ts
+++ online_scraping/lambda/scrape_wright.ts
@@ -33,6 +33,14 @@ export class ScrapeWright {
       locale: 'ko-KR',
     };
 
+    contextOptions.userAgent =
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36';
+
+    contextOptions.extraHTTPHeaders = {
+      'sec-ch-ua':
+        '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+    };
+    
     if (recordVideo) {
       const videoDir = isDocker
         ? '/tmp/videos'
